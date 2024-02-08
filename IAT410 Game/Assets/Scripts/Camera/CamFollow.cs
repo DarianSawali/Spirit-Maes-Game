25367c194dc90511a5e2f208c7264291eb28f4c9
Darian Sawali <darianaxel@gmail.com>
2024-02-08 11:39:56 -0800

    camFollow fix

--- IAT410 Game/Assets/Scripts/Camera/CamFollow.cs
+++ IAT410 Game/Assets/Scripts/Camera/CamFollow.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class CamFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 offset = new Vector3(0f, 2f, -5f);
     private Vector3 velocity = Vector3.zero;
+    [SerializeField] [Range(0.01f, 1f)] private float smoothSpeed = 0.125f;
 
-    [SerializeField][Range(0.01f, 1f)]
-    private float smoothSpeed = 0.125f;
-    
-
-    private void LateUpdate(){
+    private void LateUpdate()
+    {
         Vector3 desiredPosition = target.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+        Vector3 rotatedOffset = Quaternion.Euler(45f, 0f, 0f) * offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition + rotatedOffset, ref velocity, smoothSpeed);
+
+        transform.LookAt(target.position);
+        transform.rotation = Quaternion.Euler(45f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
     }
 }
