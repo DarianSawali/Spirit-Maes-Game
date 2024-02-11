e58a4070969b67cefa746941cf5bfdbf18a7ee9f
MichaelTj02 <mtjokrowardojo@gmail.com>
2024-02-11 12:03:56 -0800

    add possession trigger

---
+++ IAT410 Game/Assets/Scripts/Player/PossessionTrigger.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PossessionTrigger : MonoBehaviour
+{
+    public GameObject playerModel; // Assign your player model in the inspector
+    private GameObject targetAnimal = null;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && targetAnimal != null)
+        {
+            PossessAnimal(targetAnimal);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Animal")) // Make sure your animals have the "Animal" tag
+        {
+            targetAnimal = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Animal") && other.gameObject == targetAnimal)
+        {
+            targetAnimal = null;
+        }
+    }
+
+    void PossessAnimal(GameObject animal)
+    {
+        playerModel.SetActive(false); // Hide the player model
+        animal.GetComponent<AnimalControl>().enabled = true; // Enable the animal control script
+        this.enabled = false; // Optionally disable this script to prevent re-possession while already possessing an animal
+    }
+}
