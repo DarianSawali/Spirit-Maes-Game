389898f3c089bed26b642b4a817e8b30e778e4ff
Darian Sawali <darianaxel@gmail.com>
2024-02-27 12:43:55 -0800

    tileset testing for levels

---
+++ IAT410 Game/Assets/Scripts/Player/MovementManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private bool isPlayerActive = true;
+
+    private void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    public void EnablePlayerInput()
+    {
+        isPlayerActive = true;
+        playerInput.actions.FindAction("PlayerMove").Enable();
+        playerInput.actions.FindAction("SkunkMove").Disable();
+        playerInput.actions.FindAction("SkunkJump").Disable();
+    }
+
+    public void DisablePlayerInput()
+    {
+        isPlayerActive = false;
+        playerInput.actions.FindAction("PlayerMove").Disable();
+        playerInput.actions.FindAction("SkunkMove").Enable();
+        playerInput.actions.FindAction("SkunkJump").Enable();
+    }
+
+    public void EnableSkunkInput()
+    {
+        isPlayerActive = false; // Assume skunk is active
+        playerInput.actions.FindAction("PlayerMove").Disable();
+        playerInput.actions.FindAction("SkunkMove").Enable();
+        playerInput.actions.FindAction("SkunkJump").Enable();
+        playerInput.actions.FindAction("Dispossess").Enable();
+    }
+
+    public void DisableSkunkInput()
+    {
+        isPlayerActive = true; // Assume player is active
+        playerInput.actions.FindAction("SkunkMove").Disable();
+        playerInput.actions.FindAction("SkunkJump").Disable();
+        playerInput.actions.FindAction("Dispossess").Disable();
+    }
+}
