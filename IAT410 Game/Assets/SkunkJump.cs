ec326a11a67ce7459754060b4923fb512f4e49b4
Darian Sawali <darianaxel@gmail.com>
2024-03-06 23:29:43 -0800

    grav positioning

---
+++ IAT410 Game/Assets/SkunkJump.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SkunkJump : MonoBehaviour
+{
+    public float jumpForce = 1.4f;
+    public float gravity = 5f;
+    public float gravityScale = 0.2f;
+    public PlayerInput playerInput;
+    public Rigidbody rb;
+    public LayerMask groundLayer;
+    protected bool isGrounded;
+
+    private PlayerController playerControl;
+    
+    protected void Start(){
+        PlayerInput input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+
+        PlayerController playerControl = GetComponent<PlayerController>();
+    }
+
+    protected void Update()
+    {
+        isGrounded = IsGrounded();
+        if (!isGrounded)
+        {
+            rb.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+        }
+    }
+
+    public void OnJump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Debug.Log("Jump");
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        float raycastDistance = 0.1f; // Adjust this distance based on your player's size
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance))
+        {
+            if (hit.collider.CompareTag("Ground"))
+            {
+                // The player is considered grounded
+                return true;
+            }
+        }
+        return false;
+    }
+}
