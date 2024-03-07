2f5c1cb80dacad9464b016ac8c6bf487c80f7bda
Darian Sawali <darianaxel@gmail.com>
2024-03-06 21:10:31 -0800

    fixing pidgeon movement

--- IAT410 Game/Assets/Scripts/Player/PidgeonJump.cs
+++ IAT410 Game/Assets/Scripts/Player/PidgeonJump.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PidgeonJump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float jumpForce = 1.4f;
+    public float gravity = 5f;
+    public float gravityScale = 0.2f;
+    public PlayerInput playerInput;
+    public Rigidbody rb;
+    public LayerMask groundLayer;
+    protected bool isGrounded;
+
+    private PlayerController playerControl;
+    
+    protected void Start(){
+        PlayerInput input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected void Update()
     {
-        
+        isGrounded = IsGrounded();
+        if (!isGrounded)
+        {
+            rb.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+        }
+    }
+
+    public void OnPidgeonJump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Debug.Log("Jump");
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        float raycastDistance = 0.1f; // Adjust this distance based on your player's size
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance))
+        {
+            if (hit.collider.CompareTag("Ground"))
+            {
+                // The player is considered grounded
+                return true;
+            }
+        }
+        return false;
     }
 }
