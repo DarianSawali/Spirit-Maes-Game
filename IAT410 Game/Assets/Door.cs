b5715861fff9d8697787c7e715dc2a5bc90fddf4
Darian Sawali <darianaxel@gmail.com>
2024-03-11 17:20:02 -0700

    door and button triggers

---
+++ IAT410 Game/Assets/Door.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Collider doorCollider; // Collider to unlock
+    public int totalButtonCount; // Total number of buttons required to unlock the door
+    private int buttonCount = 0; // Counter for pressed buttons
+    private bool isLocked = true;
+
+    // Function called when a button is pressed
+    public void ButtonPressed()
+    {
+        buttonCount++;
+
+        // Check if all buttons are pressed
+        if (buttonCount >= totalButtonCount)
+        {
+            UnlockDoor();
+        }
+    }
+
+    // Function to unlock the door
+    private void UnlockDoor()
+    {
+        isLocked = false;
+        doorCollider.isTrigger = true; 
+    }
+
+    public void ResetDoor()
+    {
+        isLocked = true;
+        // pressedButtonCount = 0;
+        doorCollider.isTrigger = false;
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.CompareTag("Animal") || other.CompareTag("Player") || other.CompareTag("Skunk") || other.CompareTag("Pigeon") || other.CompareTag("Fish"))
+        {
+            if(doorCollider.isTrigger){
+                Debug.Log("door test working");
+            } else {
+                Debug.Log("door closed");
+            }
+
+            
+
+        }
+    }
+}
--- IAT410 Game/Assets/Scripts/ButtonTrigger.cs
+++ IAT410 Game/Assets/Scripts/ButtonTrigger.cs
@@ -5,16 +5,46 @@ using UnityEngine.UI;
 
 public class ButtonTrigger : MonoBehaviour
 {
+    public Door door; // Reference to the door script
     private TilemapSwitch tilemapSwitch;
+    public bool isPressed = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Animal") || other.CompareTag("Player"))
+        if (other.CompareTag("Animal") || other.CompareTag("Player") || other.CompareTag("Skunk") || other.CompareTag("Pigeon") || other.CompareTag("Fish"))
         {
-            TilemapSwitch tilemapSwitch = GetComponent<TilemapSwitch>();
-            tilemapSwitch.SwitchTilemaps();
-            Debug.Log("Button");
+            door.ButtonPressed();
+            isPressed = true;
+            TileSwitch();
+            Debug.Log("Button Pressed");
         }
-
     }
+
+    // private void Update(){
+    //     if(isPressed){
+    //         TileSwitch();
+    //     }
+    // }
+
+    public void TileSwitch(){
+        TilemapSwitch tilemapSwitch = GetComponent<TilemapSwitch>();
+        tilemapSwitch.SwitchTilemaps();
+    }
+
+    // private bool isPressed = false;
+    // public bool IsPressed { get { return isPressed; } }
+
+    // public Door door;
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Animal") || other.CompareTag("Player") || other.CompareTag("Skunk") || other.CompareTag("Pidgeon") || other.CompareTag("Fish"))
+    //     {
+    //         TilemapSwitch tilemapSwitch = GetComponent<TilemapSwitch>();
+    //         tilemapSwitch.SwitchTilemaps();
+    //         isPressed = true;
+    //         Debug.Log("Button Pressed");
+    //     }
+
+    // }
 }
