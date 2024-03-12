0e37686322ea92da65624cfde2bc5e2b9a9b1af3
Darian Sawali <darianaxel@gmail.com>
2024-03-12 16:59:10 -0700

    cutscene skip

--- IAT410 Game/Assets/VideoController.cs
+++ IAT410 Game/Assets/VideoController.cs
@@ -7,18 +7,37 @@ using UnityEngine.Video;
 public class VideoController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    private bool hasVideoFinished;
 
     void Start()
     {
         videoPlayer.loopPointReached += OnVideoFinished;
     }
 
+    void Update()
+    {
+        // Check for left mouse button click
+        if (Input.GetMouseButtonDown(0) && !hasVideoFinished)
+        {
+            LoadNextScene();
+        }
+    }
+
     void OnVideoFinished(VideoPlayer vp)
     {
+        hasVideoFinished = true;
+        LoadNextScene();
+    }
+
+    void LoadNextScene()
+    {
         int sceneCount = SceneManager.sceneCountInBuildSettings - 1;
-        if(SceneManager.GetActiveScene().buildIndex == sceneCount){
+        if (SceneManager.GetActiveScene().buildIndex == sceneCount)
+        {
             SceneManager.LoadScene(0);
-        } else{
+        }
+        else
+        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
