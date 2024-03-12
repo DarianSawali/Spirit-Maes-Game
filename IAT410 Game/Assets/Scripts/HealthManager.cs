641f04415c6204d216d798cef9d2330718c6cd6e
MichaelTj02 <mtjokrowardojo@gmail.com>
2024-03-12 11:54:05 -0700

    Update HealthManager.cs

--- IAT410 Game/Assets/Scripts/HealthManager.cs
+++ IAT410 Game/Assets/Scripts/HealthManager.cs
@@ -20,7 +20,7 @@ public class HealthManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int health = maxHealth;
+        health = maxHealth;
         Animator animator = GetComponent<Animator>();
     }
 
