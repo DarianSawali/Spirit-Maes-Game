877748ad0830b42ded2f384b76ee5cb6afa74472
Darian Sawali <darianaxel@gmail.com>
2024-03-24 14:35:14 -0700

    adding digs

--- IAT410 Game/Assets/Scripts/DigTrigger.cs
+++ IAT410 Game/Assets/Scripts/DigTrigger.cs
@@ -7,6 +7,7 @@ public class DigTrigger : MonoBehaviour
 {
     public DigLocation digLocation;
     private bool canDig = false;
+    private bool dug = false; 
 
     public Transform teleportLocation;
     
@@ -16,9 +17,6 @@ public class DigTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // PlayerController player = other.GetComponent<PlayerController>();
-        // Debug.Log("Player has dug.");
-
         if (other.CompareTag("Animal") || other.CompareTag("Player"))
         {
             canDig = true;
@@ -35,51 +33,6 @@ public class DigTrigger : MonoBehaviour
             canDig = false;
         }
     }
-
-    // private void OnDigPerformed(InputAction.CallbackContext context)
-    // {
-    //     if (canDig)
-    //     {
-    //         digLocation.OnDig();
-    //     }
-    // }
-
-    // public void OnDig()
-    // {
-    //     if (canDig && digLocation != null)
-    //     {
-    //         TeleportToDigLocation(digLocation.position);
-    //         Debug.Log("Dig");
-    //     }
-    // }
-
-    // public void TeleportToDigLocation(Vector3 position)
-    // {
-    //     transform.position = position;
-    // }
-
-    // private void OnEnable()
-    // {
-    //     // Enable the Dig action
-    //     PlayerInputController.Instance.PlayerControls.Player.Dig.performed += OnDigPerformed;
-    // }
-
-    // private void OnDisable()
-    // {
-    //     // Disable the Dig action
-    //     PlayerInputController.Instance.PlayerControls.Player.Dig.performed -= OnDigPerformed;
-    // }
-
-
-
-    
-    // private void Update()
-    // {
-    //     if (canDig && Input.GetKeyDown(KeyCode.F)) // Assuming 'F' is your dig input
-    //     {
-    //         digLocation.OnDig();
-    //     }
-    // }
     
 }
 
