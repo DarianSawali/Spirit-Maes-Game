a6040f8301d65796bcd6a4404cb465986b6b18f5
Darian Sawali <darianaxel@gmail.com>
2024-04-10 17:28:49 -0700

    cutsene fix

--- IAT410 Game/Assets/MainMenu.cs
+++ IAT410 Game/Assets/MainMenu.cs
@@ -23,7 +23,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         PlayerPrefs.SetInt("PlayerCurrentLives", playerLives);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
